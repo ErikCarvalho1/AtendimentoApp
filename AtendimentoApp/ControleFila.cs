c0c69a12f2afc17c8b8d2096f48dd9cefe736aba
erik.csilva7 <erik.csilva7@ITQ0675989W11-1>
2025-03-25 11:35:49 -0300

    Adicionar arquivos de projeto.

---
+++ AtendimentoApp/ControleFila.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtendimentoApp
+{
+    public class ControleFila
+    {
+        private Queue<int> filaAtendimento;
+        private int proximoNumeroGeraar = 1;
+        public ControleFila()
+        {
+            filaAtendimento = new Queue<int>();
+        }
+        public int GerarSenha()
+        {
+            filaAtendimento.Enqueue(proximoNumeroGeraar);
+            Console.WriteLine("senha gerada");//imprimir
+            return proximoNumeroGeraar++;
+        }
+        public void ChamarProximo()
+        {
+            var proximoDaFila = filaAtendimento.Dequeue();
+            Console.WriteLine($"Próxima senha:  {proximoDaFila}");
+
+        }
+        public void MostrarFila()
+        {
+            Console.WriteLine("-------------");
+            Console.Write("Senhas da Fila: ");
+            foreach (var senha in filaAtendimento)
+            {
+                Console.Write(senha + " - ");
+            }
+
+
+        }
+        public void ReiniciarFila()
+        {
+            filaAtendimento.Clear();
+        }
+     }
+}
---
+++ AtendimentoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AtendimentoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ControleFila fila = new ControleFila();
+
+            fila.GerarSenha();
+            fila.GerarSenha();
+            fila.MostrarFila();
+            fila.ChamarProximo();
+
+            fila.MostrarFila();
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
